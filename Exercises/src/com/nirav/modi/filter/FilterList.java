965b6ea1afc70f905d78f284151f75e0955a6e71
Nirav Modi <nrvmodi@gmail.com>
2015-08-04 22:45:51 +0530

    Reverse Map, FilterList

--- Exercises/src/com/nirav/modi/filter/FilterList.java
+++ Exercises/src/com/nirav/modi/filter/FilterList.java
@@ -7,6 +7,10 @@ import java.util.stream.IntStream;
 
 public class FilterList {
 
+	/**
+	 * {@link http://stackoverflow.com/questions/31808893/filter-elements-from-a-list-based-on-another-list}
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
 		List<Boolean> boolList = Arrays.asList(true, false, true, true, false);
@@ -15,6 +19,9 @@ public class FilterList {
 				.filter(i -> boolList.get(i)).mapToObj(i -> intList.get(i))
 				.collect(Collectors.toList());
 		System.out.println(collect);
+		
+		
+		
 	}
 
 }
